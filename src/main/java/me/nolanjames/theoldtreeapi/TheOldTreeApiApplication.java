3516df79bd2b64353a33a82717c93c2a68ce86d0
nj-projects <jn@jamesnolan.me>
2024-07-21 20:54:01 +0100

    initial commit

---
+++ src/main/java/me/nolanjames/theoldtreeapi/TheOldTreeApiApplication.java
@@ -0,0 +1,13 @@
+package me.nolanjames.theoldtreeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheOldTreeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheOldTreeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/nolanjames/theoldtreeapi/TheOldTreeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nolanjames.theoldtreeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheOldTreeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
