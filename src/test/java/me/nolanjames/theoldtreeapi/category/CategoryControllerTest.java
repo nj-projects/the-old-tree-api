d994282d320dcc3980d97d69fe2de68ebb8fa123
nj-projects <jn@jamesnolan.me>
2024-07-22 20:57:38 +0100

    product controller tests

---
+++ src/test/java/me/nolanjames/theoldtreeapi/category/CategoryControllerTest.java
@@ -0,0 +1,28 @@
+package me.nolanjames.theoldtreeapi.category;
+
+import me.nolanjames.theoldtreeapi.category.service.CategoryService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+class CategoryControllerTest {
+
+    @InjectMocks
+    private CategoryController categoryController;
+
+    @Mock
+    private CategoryService categoryService;
+
+    @Test
+    void createCategory() {
+
+    }
+
+    @Test
+    void listCategories() {
+    }
+
+    @Test
+    void getCategoryByName() {
+    }
+}
